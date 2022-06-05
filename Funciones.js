667b088223493ea6b21b405013ce523b42525756
efrainMedinaVega <105512538+efrainMedinaVega@users.noreply.github.com>
2022-06-05 14:23:27 -0500

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,45 @@
+document.write("<h1>Hola Mundo</h1>");
+document.write("<br><h2>Ejemplo</h2>");
+document.write("<table border=1>");
+document.write("<tr>");
+document.write("<td>");
+document.write("Nombre");
+document.write("</td>")
+document.write("<tr>");
+document.write("<td>");
+document.write("Efrain");
+document.write("</td>")
+document.write("</table>");
+let a,b,c,d;
+a=5;b=6;c=3,d=0;
+resultado=a+b+c+d;
+resta=b-c;
+divicion=a/b;
+document.write("<br>");
+document.write("El resultado de la suma es :",resultado);
+document.write("<br>");
+document.write("El resultado de la resta es :",resta);
+document.write("El resultado de la divicion es :",divicion);
+valorN=prompt("Da un valor numerico:");
+document.write("<br>");
+document.write("El valor ingresado es de:",valorN);
+valorN1=prompt("Da un valor numerico:");
+document.write("<br>");
+valorN2=prompt("Da un valor numerico:");
+document.write("<br>");
+document.write("Operaciones de Los numeros");
+document.write("<br>");
+suma1=valorN1+valorN2;
+resta1=valorN1-ValorN2;
+multiplicacion1=valorN1*valorN2;
+divicion1=valorN1/valorN2;
+document.write("El resultado de la suma es :",suma1);
+document.write("<br>");
+document.write("El resultado de la resta es:",resta1);
+document.write("<br>");
+document.write("El resultado de la multiplicacion es:",multiplicacion1);
+document.write("<br>");
+document.write("El resultado de la divicion es:",divicion1);
+
+
+
---
+++ Funsionest.js
@@ -0,0 +1,17 @@
+let nombre;
+let edad;
+let localidad;
+let profesion;
+nombre=prompt("Ingresa tu nombre completo");
+edad=prompt("Ingresa tu edad");
+localidad=prompt("Ingresa tu localidad");
+profesion=prompt("Ingresa tu profesion");
+document.write("<b>Nombre:</b>",nombre);
+document.write("<br>");
+document.write("<b>Edad:</b>",edad);
+document.write("<br>");
+document.write("<b>Localidad:</b>",localidad);
+document.write("<br>");
+document.write("<b>Profeción:</b>",profesion);
+
+
