9c3f89d4956b2fe9d3b5da01d2d4fb89d6371ba1
efrainMedinaVega <105512538+efrainMedinaVega@users.noreply.github.com>
2022-06-06 09:54:27 -0500

    Add files via upload

--- Funsionest.js
+++ Funsionest.js
@@ -2,12 +2,15 @@ let nombre;
 let edad;
 let localidad;
 let profesion;
-document.write("<table border=1>");
 nombre=prompt("Ingresa tu nombre completo");
 edad=prompt("Ingresa tu edad");
 localidad=prompt("Ingresa tu localidad");
 profesion=prompt("Ingresa tu profesion");
+document.write("<table border=1>");
 document.write("<tr>");
+document.write("<td rowspan=4>");
+document.write("<img src=img/retrato.PNG width=100px > ")
+document.write("</td>");
 document.write("<td>");
 document.write("<b>Nombre</b>");
 document.write("<td>")
@@ -31,3 +34,4 @@ document.write("<br>");
 document.write("<b>Profeción</b>");
 document.write("<td>")
 document.write(profesion);
+document.write("</table");
